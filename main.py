bf79dec4095bd10115e4bf8cc760baabdf1470ae
ybwrdwg <kasta.sten@gmail.com>
2025-06-03 18:49:55 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#import modules
 import importlib as il
 import random
 import math
 import tkinter as tk
+#def main():
 root = tk.Tk()
 root.geometry("500x500")
 root.title("txtDungeon")
 root.mainloop()
+
