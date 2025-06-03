ec91d6320ecc4047e500a4d14ae5de19690a25f3
ybwrdwg <kasta.sten@gmail.com>
2025-06-03 18:49:03 +0200

    byggde vidare på Node

--- FunTown/Node.py
+++ FunTown/Node.py
@@ -1,4 +1,33 @@
 class Node:
+    
     def __init__(self, data):
         self.data = data
-        self.next = None
+        self.next = None # Pointer to next ode
+        
+class Pair:
+    def __init__(self, data1, data2):
+        self.data1 = data1
+        self.data2 = data2
+
+class PairNode:
+    Pair("", 0)
+    def __init__(self, a):
+        self.Pair = Pair
+        self.next = None # Pointer to next PairNode
+        
+class LinkedList:
+    
+    def __init__(self):
+        self.head = None
+        
+        def __repr__(self):
+            pass
+        
+        def __contains(self):
+            pass
+        
+        def __len__(self):
+            pass
+        
+        def append(self, value):
+            pass
