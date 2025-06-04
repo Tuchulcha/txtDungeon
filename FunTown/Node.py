e3b07485700c80d47fbeb437e468952c0662b19b
Tuchulcha <tonikorkia@gmail.com>
2025-06-04 17:16:19 +0200

    Fixed style issues in FunTown/Node.py

--- FunTown/Node.py
+++ FunTown/Node.py
@@ -1,33 +1,35 @@
 class Node:
-    
     def __init__(self, data):
         self.data = data
-        self.next = None # Pointer to next ode
-        
+        self.next = None  # Pointer to next node
+
+
 class Pair:
     def __init__(self, data1, data2):
         self.data1 = data1
         self.data2 = data2
 
+
 class PairNode:
     Pair("", 0)
+
     def __init__(self, a):
         self.Pair = Pair
-        self.next = None # Pointer to next PairNode
-        
+        self.next = None  # Pointer to next PairNode
+
+
 class LinkedList:
-    
     def __init__(self):
         self.head = None
-        
-        def __repr__(self):
+
+        def __repr(self):
             pass
-        
+
         def __contains(self):
             pass
-        
-        def __len__(self):
+
+        def __len(self):
             pass
-        
+
         def append(self, value):
             pass
